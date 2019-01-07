f9faf620ff68330194c1067282bc9b34b599791c
Katie Blizzard <kblizzard11@gmail.com>
2019-01-07 23:42:44 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -3,7 +3,7 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 function init() {
   let index = 0;
   document.body.addEventListener("keydown", function(e) {
-    const key = e.detail || e.which;
+    const key = parseInt(e.detail || e.which);
     if (code[index] === key) {
       index++;
       if (index === code.length) {
